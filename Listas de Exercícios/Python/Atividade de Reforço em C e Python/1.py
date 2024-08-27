eacdcac534f578c7022d1a0bad322335c386a120
toybile <marcelino.bilibio.68@gmail.com>
2024-08-27 01:20:03 -0300

    correção -> aumento (%) and broken numbers

--- Listas de Exercícios/Python/Atividade de Reforço em C e Python/1.py
+++ Listas de Exercícios/Python/Atividade de Reforço em C e Python/1.py
@@ -3,30 +3,31 @@ print("Salário-Aumento\n")
 Name=str(input("Nome: "))
 S_A=float(input("Salário: "))
 
-A=1.0
+A=0
 
 if (S_A<401):
     A=1.15
     N_S = S_A * A
 
-elif (401<S_A<701):
+elif (401<=S_A<701):
     A=1.12
     N_S = S_A * A
 
-elif (701<S_A<1001):
+elif (701<=S_A<1001):
     A=1.1
     N_S = S_A * 1.1
 
-elif (1001<S_A<1801):
+elif (1001<=S_A<1801):
     A=1.07
     N_S = S_A * A
 
-elif (1801<S_A<2501):
+elif (1801<=S_A<2501):
     A=1.04
     N_S = S_A * A
 
-A -= 1
+A = A*100
+A -= 100
 
 print("\n============================")
-print(f" {Name}\n\nAumento: {A:2f}%\nSalário Atual: R${S_A:.2f}\nNovo Salário: R${N_S:.2f}")
+print(f" {Name}\n\nAumento: {A:.2f}%\nSalário Atual: R${S_A:.2f}\nNovo Salário: R${N_S:.2f}")
 print("==============================")
